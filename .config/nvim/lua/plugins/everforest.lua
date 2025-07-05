37b3bc28a000274f832c3a889387795a487bec55
hamishwhat <81645133+mirthfool@users.noreply.github.com>
2025-07-05 20:50:35 +1200

    fixing removing sway

--- .config/nvim/lua/plugins/everforest.lua
+++ .config/nvim/lua/plugins/everforest.lua
@@ -5,13 +5,5 @@ return {
     priority = 1000,
     config = function()
         vim.cmd("colorscheme everforest")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-        vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-        vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
     end,
 }
