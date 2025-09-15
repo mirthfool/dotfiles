d1216df2b0b24e3e16b287d585da044f4896fba9
hamishwhat <81645133+mirthfool@users.noreply.github.com>
2025-09-15 20:27:13 +1200

    idk

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,9 @@ vim.o.relativenumber = true
 vim.o.numberwidth = 1
 vim.o.signcolumn = 'yes:1'
 
-vim.o.wrap = false
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = false
 vim.o.scrolloff = 999
 vim.o.sidescrolloff = 10
 
